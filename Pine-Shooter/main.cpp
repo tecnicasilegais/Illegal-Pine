0f098ed66e37400bedb0c0e380ece28e3dc881e1
MarceloHeredia <marcelo.heredia@acad.pucrs.br>
2021-05-18 00:57:17 -0300

    testing texture + image bg

--- Pine-Shooter/main.cpp
+++ Pine-Shooter/main.cpp
@@ -29,11 +29,15 @@ using namespace std;
 #include <GL/glut.h>
 #endif
 
+#include <SOIL/SOIL.h>
+
+#include "image_libs/TextureClass.h"
+#include "image_libs/ImageClass.h"
 #include "headers/Ponto.h"
 #include "headers/Poligono.h"
-
 #include "headers/Temporizador.h"
 
+// Global Variables
 Temporizador T;
 double accum_delta_t = 0;
 
@@ -42,16 +46,53 @@ Poligono A;
 vector<Ponto> B, binit;
 
 //Limites logicos da area de desenho
-Ponto Min, Max;
-Ponto Meio, Terco, Largura;
-
-bool desenha = false;
+Ponto Min, Max, Largura;
 
 float angle = 0.0;
 float walk = 0.0;
+double n_frames = 0;
+double tempo_total = 0;
 
-const string f_path = "data/";
+ImageClass bg;
 
+
+GLfloat AspectRatio, AngY=0;
+GLuint TEX1;
+
+void initTexture (void)
+{
+    TEX1 = LoadTexture ("img/maiscol.png");
+}
+
+void DesenhaCubo ()
+{
+    glBegin ( GL_QUADS );
+    // Front Face
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(-1.0f, -1.0f,  1.0f);
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f( 1.0f, -1.0f,  1.0f);
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f( 1.0f,  1.0f,  1.0f);
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f(-1.0f,  1.0f,  1.0f);
+    glEnd();
+
+}
+void PosicUser()
+{
+    // Set the clipping volume
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(90,AspectRatio,0.01,200);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(0, 0, 0,
+              0,0,-8,
+              0.0f,1.0f,0.0f);
+
+}
 /**
  * Le o arquivo nome e popula o poligono P
  */
@@ -89,8 +130,19 @@ void le_poligono(const string nome, Poligono &P)
 
 void init()
 {
+
+    glShadeModel(GL_SMOOTH);
+    glColorMaterial ( GL_FRONT, GL_AMBIENT_AND_DIFFUSE );
+    glEnable(GL_DEPTH_TEST);
+    glEnable ( GL_CULL_FACE );
+
+    initTexture();
+
+    string nome = "./img/Pine-BG.png";
+    if(!bg.Load(nome.c_str())){exit(666);}
     // Define a cor do fundo da tela (AZUL)
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
 
     A.insere_vertice(Ponto(20,20));
     A.insere_vertice(Ponto(20,25));
@@ -132,8 +184,6 @@ void init()
 
 }
 
-double n_frames = 0;
-double tempo_total = 0;
 
 void animate()
 {
@@ -166,6 +216,7 @@ void animate()
 void reshape(int w, int h)
 {
     // Reset the coordinate system before modifying
+    AspectRatio = 1.0f * w / h;
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     // Define a area a ser ocupada pela area OpenGL dentro da Janela
@@ -175,6 +226,7 @@ void reshape(int w, int h)
             Min.y, Max.y,
             0, 1);
 
+    //PosicUser();
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 }
@@ -202,12 +254,40 @@ void display(void)
 {
 
     // Limpa a tela coma cor de fundo
-    glClear(GL_COLOR_BUFFER_BIT);
+    //glClear(GL_COLOR_BUFFER_BIT);
+    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 
     // Define os limites logicos da area OpenGL dentro da Janela
+   // glMatrixMode(GL_MODELVIEW);
+  //  glLoadIdentity();
+
+
+    //glClear(GL_COLOR_BUFFER_BIT); //Limpa a tela coma cor de fundo
+    glMatrixMode(GL_PROJECTION);//Define os limites logicos da area OpenGL dentro da Janela
+    glLoadIdentity();
+    glOrtho(Min.x, Max.x, Min.y, Max.y,0, 1);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
+    float zoomH = (glutGet(GLUT_WINDOW_WIDTH))/(double)bg.SizeX();
+    float zoomV = (glutGet(GLUT_WINDOW_HEIGHT))/(double)bg.SizeY();
+    bg.SetZoomH(zoomH);
+    bg.SetZoomV(zoomV);
+    bg.SetPos(0, 0);
+    bg.Display();
 
+
+
+    glPushMatrix();
+    PosicUser();
+    glTranslatef ( -2.0f, 2.0f, -5.0f );
+    //glRotatef(AngY,0,1,0);
+    //glColor3f(0.0f,0.6f,0.0f); // Verde
+    glBindTexture (GL_TEXTURE_2D, TEX1);
+    DesenhaCubo();
+    glPopMatrix();
+
+
+/*
     glPushMatrix();
     glTranslatef(22.5+walk, 22.5, 0);
     glRotatef(angle, 0,0,1);
@@ -243,7 +323,7 @@ void display(void)
         glVertex3f(Vertice.x, Vertice.y, Vertice.z);
     }
     glEnd();
-    //imprime p1_new e p2_new
+    //imprime p1_new e p2_new*/
     glutSwapBuffers();
 }
 
@@ -281,7 +361,6 @@ void keyboard(unsigned char key, int x, int y)
             conta_tempo(3);
             break;
         case ' ':
-            desenha = !desenha;
             break;
         case 'a':
             walk += -1;
@@ -325,49 +404,22 @@ int main(int argc, char** argv)
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH | GLUT_RGB);
     glutInitWindowPosition(0, 0);
 
-    // Define o tamanho inicial da janela grafica do programa
-    glutInitWindowSize(800, 600);
+    glutInitWindowSize(1000, 600);
 
-    // Cria a janela na tela, definindo o nome da
-    // que aparecera na barra de titulo da janela.
     glutCreateWindow("Primeiro Programa em OpenGL");
 
-    // executa algumas inicializacoes
     init();
 
-    // Define que o tratador de evento para
-    // o redesenho da tela. A funcao "display"
-    // sera chamada automaticamente quando
-    // for necessario redesenhar a janela
     glutDisplayFunc(display);
 
-    // Define que o tratador de evento para
-    // o invalidacao da tela. A funcao "display"
-    // sera chamada automaticamente sempre que a
-    // maquina estiver ociosa (idle)
     glutIdleFunc(animate);
 
-    // Define que o tratador de evento para
-    // o redimensionamento da janela. A funcao "reshape"
-    // sera chamada automaticamente quando
-    // o usuario alterar o tamanho da janela
     glutReshapeFunc(reshape);
 
-    // Define que o tratador de evento para
-    // as teclas. A funcao "keyboard"
-    // sera chamada automaticamente sempre
-    // o usuario pressionar uma tecla comum
     glutKeyboardFunc(keyboard);
 
-    // Define que o tratador de evento para
-    // as teclas especiais(F1, F2,... ALT-A,
-    // ALT-B, Teclas de Seta, ...).
-    // A funcao "arrow_keys" sera chamada
-    // automaticamente sempre o usuario
-    // pressionar uma tecla especial
     glutSpecialFunc(arrow_keys);
 
-    // inicia o tratamento dos eventos
     glutMainLoop();
 
     return 0;
